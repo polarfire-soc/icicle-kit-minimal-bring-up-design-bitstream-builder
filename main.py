c6363ef2d49045b8e49efc14456abc4ccc83a76b
M31864 <hugh.breslin@microchip.com>
2022-01-11 10:25:33 +0000

    cleaned up comments and added a check for tool paths

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ import shutil
 import subprocess
 
 
-# Update to also init enviroment variables
+# Creates required folders and removes artifacts before beginning
 def init_workspace():
+    # Create the sources folder to clone into if it doesn't exist (any existing source folders are handled in the clone_sources function)
     if not os.path.exists("./sources"):
         os.mkdir("./sources")
 
+    # delete the output folder if it exists to remove old artifacts
     if os.path.exists("./output"):
         shutil.rmtree('./output')
+
+    # Create each output sub-directory
     os.mkdir("./output")
     os.mkdir("./output/MSS")
     os.mkdir("./output/HSS")
@@ -22,34 +26,49 @@ def init_workspace():
     os.mkdir("./output/payload")
     os.mkdir("./output/clients")
     os.mkdir("./output/final-files")
-    # delete all folders in workspace
 
 
 # clones the sources specified in the sources.yaml file
 def clone_sources(source_list):
     source_directories = {}
-    with open(source_list) as f: # open the yaml file passed as an arg
+    with open(source_list) as f:  # open the yaml file passed as an arg
         data = yaml.load(f, Loader=yaml.FullLoader)
 
         keys = data.keys()
-        for source in keys: # each entry in the file is a source
-            if "git" in data.get(source).get("type"): # Check if this is a git source
-                if os.path.exists(os.path.join("./sources", source)): # Check if we've already cloned the repo
-                    repo = git.Repo.init(os.path.join("./sources", source)) # set up repo
-                    repo.git.checkout(data.get(source).get("branch")) # checkout the branch from the yaml file
-                    repo.remotes.origin.pull() # pull changes
-                else:
-                    repo = git.Repo.clone_from(data.get(source).get("link"), os.path.join("./sources", source), branch=data.get(source).get("branch")) # if we don't already have the repo, clone it
-                if "commit" in data.get(source): # check if a specific commit from this branch is required
-                    repo.git.checkout(data.get(source).get("commit")) # check out a specific commit
-            elif "zip" in data.get(source).get("type"): # Check if this is source is a url to a zip
-                if os.path.exists(os.path.join("./sources", source)): # if we already have a source of the same name delete it - can't check versions
-                    shutil.rmtree(os.path.join("./sources", source))
-                r = requests.get(data.get(source).get("link")) # download zip
-                z = zipfile.ZipFile(io.BytesIO(r.content)) # extract zip
-                z.extractall(os.path.join("./sources", source)) # save contents
-            source_directories[source] = os.path.join("./sources", source) # Generate a dictionary of all of the sources that were cloned
+        # each entry in the file is a source
+        for source in keys:
 
+            # Check if this is a git source
+            if "git" in data.get(source).get("type"):
+
+                # Check if we've already cloned the repo
+                if os.path.exists(os.path.join("./sources", source)):
+                    repo = git.Repo.init(os.path.join("./sources", source))  # set up repo
+                    repo.git.checkout(data.get(source).get("branch"))  # checkout the branch from the yaml file
+                    repo.remotes.origin.pull()  # pull changes
+
+                # We don't already have the repo, clone it
+                else:
+                    repo = git.Repo.clone_from(data.get(source).get("link"), os.path.join("./sources", source),
+                                               branch=data.get(source).get("branch"))
+
+                # check if a specific commit from this branch is required
+                if "commit" in data.get(source):
+                    repo.git.checkout(data.get(source).get("commit"))  # check out a specific commit
+
+            # Check if this is source is a url to a zip
+            elif "zip" in data.get(source).get("type"):
+
+                # if we already have a source of the same name delete it - can't check versions
+                if os.path.exists(os.path.join("./sources", source)):
+                    shutil.rmtree(os.path.join("./sources", source))
+                r = requests.get(data.get(source).get("link"))  # download zip
+                z = zipfile.ZipFile(io.BytesIO(r.content))  # extract zip
+                z.extractall(os.path.join("./sources", source))  # save contents
+            source_directories[source] = os.path.join("./sources",
+                                                      source)  # Generate a dictionary of all of the sources that were cloned
+
+    # return the dictionary of sources
     return source_directories
 
 
@@ -62,12 +81,16 @@ def make_mss_config(mss_configurator, config_file, output_dir):
 def make_hss(hss_source):
     # Update XML in HSS project
     os.remove(os.path.join(hss_source, "boards/mpfs-icicle-kit-es/soc_fpga_design/xml/ICICLE_MSS_mss_cfg.xml"))
-    shutil.copyfile("./output/MSS/test_mss_mss_cfg.xml", os.path.join(hss_source, "boards/mpfs-icicle-kit-es/soc_fpga_design/xml/ICICLE_MSS_mss_cfg.xml"))
+    shutil.copyfile("./output/MSS/test_mss_mss_cfg.xml",
+                    os.path.join(hss_source, "boards/mpfs-icicle-kit-es/soc_fpga_design/xml/ICICLE_MSS_mss_cfg.xml"))
+
     # remove buggy file in project and replace
     os.remove(os.path.join(hss_source, "include/hss_debug.h"))
     shutil.copyfile("recipes/HSS/hss_debug.h", os.path.join(hss_source, "./include/hss_debug.h"))
+
     # Add custom build configuration
     shutil.copyfile("recipes/HSS/spi-boot-hss.config", os.path.join(hss_source, "./.config"))
+
     # create softconsole headless call
     workspace = " --workspace=" + os.path.join(os.getcwd(), "output", "bare-metal", "workspace")
     project = " --import=" + hss_source
@@ -75,8 +98,10 @@ def make_hss(hss_source):
     native = " --native"
     softconsole_call = workspace + project + build + native
     os.system(softconsole_headless + softconsole_call)
+
     # copy the build artifact to the output directory
-    shutil.copyfile("./sources/HSS/Default/bootmode1/hss-envm-wrapper-bm1-p0.hex", "./output/HSS/hss-envm-wrapper-bm1-p0.hex")
+    shutil.copyfile("./sources/HSS/Default/bootmode1/hss-envm-wrapper-bm1-p0.hex",
+                    "./output/HSS/hss-envm-wrapper-bm1-p0.hex")
 
 
 # builds a bare metal project using softconsole
@@ -87,10 +112,14 @@ def make_bare_metal(softconsole_headless, bare_metal_source):
     build = " --build=mpfs-mmuart-interrupt/DDR-Release"
     native = " --native"
     softconsole_call = workspace + project + build + native
+
     # build the project
     os.system(softconsole_headless + softconsole_call)
+
     # copy the project artifact to the output directory
-    shutil.copyfile("./sources/bare-metal-examples/driver-examples/mss/mss-mmuart/mpfs-mmuart-interrupt/DDR-Release/mpfs-mmuart-interrupt.elf", "./output/bare-metal/mpfs-mmuart-interrupt.elf" )
+    shutil.copyfile(
+        "./sources/bare-metal-examples/driver-examples/mss/mss-mmuart/mpfs-mmuart-interrupt/DDR-Release/mpfs-mmuart-interrupt.elf",
+        "./output/bare-metal/mpfs-mmuart-interrupt.elf")
 
 
 # Generates a HSS payload using the HSS payload generator using a config file
@@ -98,10 +127,13 @@ def make_hss_payload(payload_generator, config, destination):
     # backup current directory and change in to the payload generator directory
     top_dir = os.getcwd()
     os.chdir(payload_generator)
+
     # make sure the tool is executable (we download and extract it as a zip source)
     os.system("chmod +x ./hss-payload-generator")
+
     # Generate the payload with the output going to the output directory
     os.system("./hss-payload-generator -c " + config + " " + destination)
+
     # Change back to the original directory
     os.chdir(top_dir)
 
@@ -117,20 +149,34 @@ if __name__ == '__main__':
     user_home = os.path.expanduser("~")
 
     # Tool path variables
-    libero_install_directory = "/usr/local/microsemi/Libero_SoC_v2021.2/Libero/" # This script is design to use Libero 2021.2
+    libero_install_directory = "/usr/local/microsemi/Libero_SoC_v2021.2/Libero/"  # This script is design to use Libero 2021.2
     mss_configurator = os.path.join(libero_install_directory, "bin64/pfsoc_mss")
     libero = os.path.join(libero_install_directory, "bin/libero")
-    softconsole_headless = user_home + "/Microchip/SoftConsole-v2021.3/eclipse/softconsole-headless" # This script is designed to use SoftConsole v2021.3
+    softconsole_headless = user_home + "/Microchip/SoftConsole-v2021.3/eclipse/softconsole-headless"  # This script is designed to use SoftConsole v2021.3
+
+    if not os.path.isfile(mss_configurator):
+        print("Error: path to MSS configurator is incorrect!")
+        exit()
+
+    if not os.path.isfile(libero):
+        print("Error: path to Libero installation is incorrect!")
+        exit()
+
+    if not os.path.isfile(softconsole_headless):
+        print("Error: path to SoftConsole headless is incorrect!")
+        exit()
 
     # Set enviroment variables required by tools
 
     # SoftConsole #
-    os.environ["PATH"] = os.environ["PATH"] + ":" + user_home + "/Microchip/SoftConsole-v2021.3/python/bin:" + user_home + "/Microchip/SoftConsole-v2021.3/riscv-unknown-elf-gcc/bin:" + user_home + "/Microchip/SoftConsole-v2021.3/eclipse/jre/bin"
+    os.environ["PATH"] = os.environ[
+                             "PATH"] + ":" + user_home + "/Microchip/SoftConsole-v2021.3/python/bin:" + user_home + "/Microchip/SoftConsole-v2021.3/riscv-unknown-elf-gcc/bin:" + user_home + "/Microchip/SoftConsole-v2021.3/eclipse/jre/bin"
     os.environ["SC_INSTALL_DIR"] = user_home + "/Microchip/SoftConsole-v2021.3"
     os.environ["FPGENPROG"] = "/usr/local/microsemi/Libero_SoC_v2021.2/Libero/bin64/fpgenprog"
 
     # Libero #
-    os.environ["LM_LICENSE_FILE"] = "1703@molalla.microsemi.net:1800@molalla.microsemi.net:1717@molalla.microsemi.net:1717@wilkie.microsemi.net:1800@wilkie.microsemi.net"
+    os.environ[
+        "LM_LICENSE_FILE"] = "1703@molalla.microsemi.net:1800@molalla.microsemi.net:1717@molalla.microsemi.net:1717@wilkie.microsemi.net:1800@wilkie.microsemi.net"
 
     print("Initializing workspace")
     init_workspace()
@@ -148,8 +194,10 @@ if __name__ == '__main__':
     make_bare_metal(softconsole_headless, sources["bare-metal-examples"])
 
     print("Generating HSS payload")
-    make_hss_payload(os.path.join(sources["HSS-payload-generator"], "hss-payload-generator/binaries/"), os.path.join(os.getcwd(),
-                                                                                                                     "recipes/hss-payload/config.yaml"), os.path.join(os.getcwd(), "output/payload/spi.bin"))
+    make_hss_payload(os.path.join(sources["HSS-payload-generator"], "hss-payload-generator/binaries/"),
+                     os.path.join(os.getcwd(),
+                                  "recipes/hss-payload/config.yaml"),
+                     os.path.join(os.getcwd(), "output/payload/spi.bin"))
 
     print("Generating Libero project")
     make_libero_project(libero, "./recipes/libero-project/generate-project.tcl")
